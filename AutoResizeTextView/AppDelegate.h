c2dfa3f10886c7e0c262ac0038eb7d8a6ca293ea
Sam Fung <sam.fung@gmail.com>
2014-05-26 10:14:08 -0700

    Initial Commit

---
+++ AutoResizeTextView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoResizeTextView
+//
+//  Created by bbpan on 5/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AutoResizeTextView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AutoResizeTextView
+//
+//  Created by bbpan on 5/26/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
